22299027645654170f50c1c0c50b2e52ad53aed8
Ashish Amar <ashishamar1999@gmail.com>
2023-01-22 22:48:05 +0530

    added code for image generation & text completion

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ import os
 import openai
 from dotenv import load_dotenv
 
+def text_completion(promptText):
+    response = openai.Completion.create(
+        model="code-davinci-002",
+        prompt=promptText,
+        temperature=1,
+        max_tokens=30,
+        top_p=1.0,
+        n=3
+    )
+    print(response)
+    
+def image_generation(promptText):
+    response = openai.Image.create(
+        prompt=promptText,
+        n=1,
+        response_format="url"
+    )
+    print(response)
+
 if __name__ == "__main__":
     load_dotenv()
     openai.organization = os.getenv("OPENAI_ORG_KEY")
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    print(openai.Model.list())
+    
+    text_completion(input("Enter prompt for text completion:: "))
+    image_generation(input("Enter text for image generation:: "))
--- notes.md
+++ notes.md
@@ -11,3 +11,9 @@ Parameters for request are :
 - top_p - of the generated tokens will filter and give according to top probability value.
 ![top_p.png](images/topp.png)
 - n - is the number of completions to generate for each prompt.
+
+<hr />
+
+1. Even if temp was given 1, the suggestions were kinda weird, 0.9 suggestions had mixed languages. I think it was the use of top_p and temp. Need to figure out how to control those to produce desired results.
+
+2. image generation was a bit confused about a few prompts, example i gave the prompt spongebob eating patrick and got the exact opposite, patrick with spongebob's hand in his mouth. Also, found out there's a safety check on the image generation as well. We cannot use it to generate celebrities images like "modi eating popcorn".
