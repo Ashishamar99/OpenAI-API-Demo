40a3bcdccecb566d023e1053a699bc5df900f7ec
Ashish Amar <ashishamar1999@gmail.com>
2023-01-22 14:44:39 +0530

    modified main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import openai
 from dotenv import load_dotenv
 
-load_dotenv()
-openai.organization = os.getenv("OPENAI_ORG_KEY")
-openai.api_key = os.getenv("OPENAI_API_KEY")
-print(openai.Model.list())
+if __name__ == "__main__":
+    load_dotenv()
+    openai.organization = os.getenv("OPENAI_ORG_KEY")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    print(openai.Model.list())
